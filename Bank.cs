ad3aa33210633266b7ad40b69c4bc698cb7d40c2
cjgochanour <cjgochanour@gmail.com>
2022-03-03 14:43:19 -0600

    added luck calculator

--- Bank.cs
+++ Bank.cs
@@ -1,8 +1,15 @@
+using System;
+
 namespace Heist
 {
     public class Bank
     {
         private int _difficulty = 100;
         public int Difficulty { get { return _difficulty; } }
+        public void CalculateLuck()
+        {
+            int Random = new Random().Next(-10, 11);
+            _difficulty += Random;
+        }
     }
 }
